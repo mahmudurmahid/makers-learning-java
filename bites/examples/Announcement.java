18c2db395bb3fca4fa62ad0fe629a5d5fb592063
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 14:15:38 +0000

    added apology method with full logic, also printed to main

--- bites/examples/Announcement.java
+++ bites/examples/Announcement.java
@@ -4,6 +4,7 @@ public class Announcement {
     public static void main(String[] args) {
         System.out.println(Announcement.celebration());
         System.out.println(Announcement.changeOfPlan());
+        System.out.println(Announcement.apology());
     }
 
     public static String celebration() {
@@ -12,5 +13,9 @@ public class Announcement {
 
     public static String changeOfPlan() {
         return "There has been a change of plans";
+    }
+
+    public static String apology() {
+        return "I am very sorry";
     }
 }
