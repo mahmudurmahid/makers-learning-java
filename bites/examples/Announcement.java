93de6a3cfb48e9398cd401a722600fe96edfcde5
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 15:33:36 +0000

    changed all announcement class methods to use instance methods

--- bites/examples/Announcement.java
+++ bites/examples/Announcement.java
@@ -2,30 +2,31 @@ package bites.examples;
 
 public class Announcement {
     public static void main(String[] args) {
-        System.out.println(Announcement.celebration());
-        System.out.println(Announcement.changeOfPlan());
-        System.out.println(Announcement.apology());
-        System.out.println(Announcement.politeNotice());
-        System.out.println(Announcement.helpNeeded());
+        Announcement myAnnouncement = new Announcement();
+        System.out.println(myAnnouncement.celebration());
+        System.out.println(myAnnouncement.changeOfPlan());
+        System.out.println(myAnnouncement.apology());
+        System.out.println(myAnnouncement.politeNotice());
+        System.out.println(myAnnouncement.helpNeeded());
     }
 
-    public static String celebration() {
+    public String celebration() {
         return "Happy belated birthday";
     }
 
-    public static String changeOfPlan() {
+    public String changeOfPlan() {
         return "There has been a change of plans";
     }
 
-    public static String apology() {
+    public String apology() {
         return "I am very sorry";
     }
 
-    public static String politeNotice() {
+    public String politeNotice() {
         return "May I kindly ask you to...";
     }
 
-    public static String helpNeeded() {
+    public String helpNeeded() {
         return "I need your help with...";
     }
 }
