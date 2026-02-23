340c8fcc5b6c71b6303a184fa4a7050e6182e60f
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 13:13:05 +0000

    added logic behind class

--- bites/examples/JavaQuiz.java
+++ bites/examples/JavaQuiz.java
@@ -1,4 +1,10 @@
 package bites.examples;
 
 public class JavaQuiz {
+    public static void main(String[] args) {
+        System.out.println("Q1: Yes, all Java code has to be inside a class");
+        System.out.println("Q2: Compilation is the process of converting human-readable Java source code into bytecode that JVM (Java Virtual Machine) can run");
+        System.out.println("Q3: The main method is automatically called when a Java program is run. Every program must have one and only one main method otherwise the program will not compile");
+        System.out.println("Q4: is a return type for a method definition that returns no value");
+    }
 }
