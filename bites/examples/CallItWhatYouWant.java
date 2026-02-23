53b14e25e4b7a81e5b51f28eac8bf5ab8bbb7ad9
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 13:17:31 +0000

    added challenge at the bottom of the writing java page

---
+++ bites/examples/CallItWhatYouWant.java
@@ -0,0 +1,8 @@
+package bites.examples;
+
+public class CallItWhatYouWant {
+    public static void main(String[] arg) {
+        System.out.println("This is a challenge set by Makers at the bottom of the writing java page");
+        System.out.println("I am meant to explain what is being printed");
+    }
+}
