61f84ea208353cd8e961a4003dbc3426d8777a71
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 14:30:17 +0000

    complete mayaAngelou method

--- bites/examples/Quotes.java
+++ bites/examples/Quotes.java
@@ -3,9 +3,14 @@ package bites.examples;
 public class Quotes {
     public static void main(String[] args) {
         System.out.println(Quotes.albertEinstein());
+        System.out.println(Quotes.mayaAngelou());
     }
 
     public static String albertEinstein() {
         return "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+    }
+
+    public static String mayaAngelou() {
+        return "You will face many defeats in life, but never let yourself be defeated.";
     }
 }
