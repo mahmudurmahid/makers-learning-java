688f5004e020fea37ea5af7e9de2ff736b4ebaff
mahmudur mahid <m.mahid@icloud.com>
2026-02-23 14:28:53 +0000

    added boiler plate Quotes class, with complete albertEinstein method, connected to main method

---
+++ bites/examples/Quotes.java
@@ -0,0 +1,11 @@
+package bites.examples;
+
+public class Quotes {
+    public static void main(String[] args) {
+        System.out.println(Quotes.albertEinstein());
+    }
+
+    public static String albertEinstein() {
+        return "Life is like riding a bicycle. To keep your balance, you must keep moving.";
+    }
+}
