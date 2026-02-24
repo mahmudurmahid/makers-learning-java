d58c9860626ebffeae2c3b71a0a578d4de109b79
mahmudur mahid <m.mahid@icloud.com>
2026-02-24 12:15:03 +0000

    added friendlyTotal instance method to Order class

--- bites/examples/Order.java
+++ bites/examples/Order.java
@@ -19,11 +19,17 @@ public class Order {
         return this.total;
     }
 
+    public String friendlyTotal() {
+        return String.format("Thanks! The total value of your order is %s", this.total);
+    }
+
     public static void main(String[] args) {
         Order order = new Order();
         order.addAmount(10);
         order.addDiscount(1);
         Integer total = order.getTotal();
-        System.out.println(total);
+        String friendlyTotal = order.friendlyTotal();
+
+        System.out.println(friendlyTotal);
     }
 }
