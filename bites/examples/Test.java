64627b92338911b22b7904feb48d7ed94a1a5fc7
mahmudur mahid <m.mahid@icloud.com>
2026-02-25 15:17:19 +0000

    test file - testing

--- bites/examples/Test.java
+++ bites/examples/Test.java
@@ -1,4 +1,26 @@
 package bites.examples;
 
 public class Test {
+    public static void main(String[] args) {
+        System.out.println(quoteOne());
+        Test.quoteTwo();
+        Test.quoteThree("QuoteThree - test");
+        System.out.println(quoteFour());
+    }
+
+    public static String quoteOne() {
+        return "QuoteOne - test";
+    }
+
+    public static void quoteTwo() {
+        System.out.println("QuoteTwo - test");
+    }
+
+    public static void quoteThree(String quote) {
+        System.out.println(quote);
+    }
+
+    public static String quoteFour() {
+        return "QuoteFour - test";
+    }
 }
