195da8140344d64b1518e1e836e926793b661abb
mahmudur mahid <m.mahid@icloud.com>
2026-02-26 11:31:53 +0000

    alternative solution with more examples

--- bites/examples/FizzBuzz.java
+++ bites/examples/FizzBuzz.java
@@ -4,17 +4,21 @@ import java.util.Scanner;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-        Integer number = 1;
+        System.out.println(FizzBuzz.play(1));
+        System.out.println(FizzBuzz.play(3));
+        System.out.println(FizzBuzz.play(5));
+        System.out.println(FizzBuzz.play(15));
+    }
 
+    private static String play(Integer number) {
         if (number % 3 == 0 && number % 5 == 0) {
-            System.out.println("FizzBuzz");
-        } else if (number % 5 == 0) {
-            System.out.println("Buzz");
+            return "FizzBuzz";
         } else if (number % 3 == 0) {
-            System.out.println("Fizz");
-        }
-        else {
-            System.out.println("Keep up, sarn");
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return "Try again, sunshine";
         }
     }
 }
